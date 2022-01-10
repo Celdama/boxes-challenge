cf90759fdda887e3d2e31e429a176603a319e43c
Celdama <celdama.dev@gmail.com>
2022-01-10 09:59:05 +0100

    first part of challenge done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import React, { useState } from 'react';
+import boxes from './boxes';
+import './app.css';
+
+const App = () => {
+  const [squares, setSquares] = useState(boxes);
+
+  const squareElements = squares.map(({ id }) => (
+    <div key={id} className='box'></div>
+  ));
+
   return (
-    <div className='App'>
-      <h1>boxes</h1>
-    </div>
+    <main>
+      <h1>Boxes will go here</h1>
+      <div className='container'>{squareElements}</div>
+    </main>
   );
-}
+};
 
 export default App;
